92015d9d7d7c3bcd8851aff9250ef9c8cdcba284
Patchapon Rochananan <patchapon.r@ku.th>
2021-08-07 06:01:02 +0700

    add Leaderboard methods and UsersApi with Admin header

--- src/views/Leaderboard.vue
+++ src/views/Leaderboard.vue
@@ -1,15 +1,38 @@
 <template>
   <div>
       <h1>for admin</h1>
+      <p>table of users and points in a given date range</p>
+      <p>sort by points and accuPoints</p>
   </div>
 </template>
 
 <script>
 import AuthUser from "@/store/AuthUser"
+import AuthService from "@/services/AuthService"
+import UsersApiStore from "@/store/UsersApi"
 export default {
+    data() {
+        return {
+            users: []
+        }
+    },
     methods: {
+        async fetchUsers(){
+            await PokedexApiStore.dispatch("fetchUsers")
+            this.users = UsersApiStore.getters.users
+        },
         isAuthen() {
             return AuthUser.getters.isAuthen
+        },
+        isAdmin() {
+            return AuthService.isAdministration()
+        }
+    },
+    created(){
+        this.fetchUsers()
+        if(!this.isAdmin) {
+            this.$swal("You have no permission","","warning")
+            this.$router.push("/")
         }
     }
 }
