c4aaf600ef4a598ef7605b33781eb0d7cdcdbf02
thanakit <thanakit.kit@ku.th>
2021-08-10 10:31:55 +0700

    stock reward

--- src/services/RewardService.js
+++ src/services/RewardService.js
@@ -5,9 +5,6 @@ import AuthUser from '@/store/AuthUser'
 const api_endpoint = process.env.VUE_APP_API_ENDPOINT || "http://localhost:1337"
 
 export default {
-  add(){
-    
-  },
 
   async redeemPoint(Name, amount) {
     let url = api_endpoint + "/point-uses"
